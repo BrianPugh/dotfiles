f05b535d1470ab125561dbfd4b756faaf92c0113
Brian Pugh <bnp117@gmail.com>
2022-01-01 18:20:45 -0800

    more aggressive gitgutter colors

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -6,9 +6,9 @@ vim.cmd[[highlight Pmenu guibg=#112C85]]  -- linked to floating dialogues
 
 -- Gutter Stuff
 vim.cmd[[highlight SignColumn guibg=#303030]] -- gutter
-vim.cmd[[highlight GitGutterDelete guifg=red guibg=#303030]] -- 
-vim.cmd[[highlight GitGutterChange guifg=blue guibg=#303030]] -- 
-vim.cmd[[highlight GitGutterAdd guifg=green guibg=#303030]] -- 
+vim.cmd[[highlight GitGutterDelete guifg=#FF0000 guibg=#303030 gui=bold cterm=bold]] -- 
+vim.cmd[[highlight GitGutterChange guifg=#0000FF guibg=#303030 gui=bold cterm=bold]] -- 
+vim.cmd[[highlight GitGutterAdd guifg=#00FF00 guibg=#303030 cterm=bold]] -- 
 
 -- CoC
 vim.cmd[[highlight CocUnusedHighlight guibg=#505050]] -- 
