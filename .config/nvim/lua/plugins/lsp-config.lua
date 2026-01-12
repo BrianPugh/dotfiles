8e48e733c5579744e6ab3d69539d4b6e6bd4cd46
Brian Pugh <bnp117@gmail.com>
2026-01-12 15:29:00 -0500

    misc

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -2,15 +2,17 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      ["*"] = {
+        capabilities = {
+          signatureHelp = false, -- Disable signature help
+        },
+      },
       clangd = {
         filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "hpp" },  -- notably: remove .proto
       },
     },
     diagnostics = {
       virtual_text = false, -- Disable virtual text (code issues)
-    },
-    capabilities = {
-      signatureHelp = false, -- Disable signature help
     },
   },
 }
